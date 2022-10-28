dc796285ded87fd288075f76a0fc5a33575e7a65
Jan Magnuszewski <janekmagnuszewski@o2.pl>
2022-10-28 16:18:32 +0100

    idw from census vars

--- data_prep.R
+++ data_prep.R
@@ -9,8 +9,9 @@ library(rgeos)
 library(geosphere)
 library(gstat)
 library(spdep)
-
- #### Data imports ####
+library(VIM)
+ 
+#### Data imports ####
 # geographic data
 sa1 <- st_read("data/geographic/auckland_urban_sa1s.gpkg")
 #sa1_cent <- st_read("data/geographic/wellington_urban_polygons_sa1s.gpkg")
@@ -33,7 +34,6 @@ tm_shape(grid) +
   tm_shape(stations) +
   tm_dots(col="red")
 
-head(census)
 #### Adding Census vars ####
 census <- census %>%
   mutate(code = as.character(code)) %>%
@@ -41,30 +41,33 @@ census <- census %>%
   mutate(median_income = as.numeric(median_income)) %>%
   mutate(maori_desc = as.numeric(maori_desc)) %>%
   mutate(pop_usual = as.numeric(pop_usual)) %>%
-  mutate(maori_pr = maori_desc/pop_usual)
+  mutate(maori_pr = maori_desc/pop_usual) %>%
+  mutate(dampness = as.numeric(as.factor(dampness)))
 
+head(census)
 sa1_all <- left_join(sa1, census, by = c("SA12018_V1"="code"))
+
 #sa1_all = subset(sa1_all, select = -c(maori_desc))
 head(sa1_all)
 
 tm_shape(sa1_all) +
   tm_fill(col="maori_pr",
-          style = "kmeans", palette = "Reds") #+
-  #tm_borders("transparent")
+          style = "kmeans", palette = "Reds")
+
 ##### Impute missing values #####
-length(sa1_all[is.na(sa1_all),])
+summary(aggr(census))
+sa1_all$maori_pr[is.na(sa1_all$maori_pr)] <- 0
 
-sa1_all <- mutate(sa1_all, dampness = as.numeric(as.character(dampness)))
-sa1 <- SpatialPoints(sa1)
-knn5 <- knn2nb(knearneigh(sa1, k = 5))
-
-sapply(1:length(knn5), function(N){mean(dampness[N])})
-
-sa1s_na_damp <- sa1_all[which(is.na(sa1_all$dampness)),]
-sa1s_na_damp$dampness
+#imputation by neighbouring values - not working yet
+#sa1_all <- mutate(sa1_all, dampness = as.numeric(as.character(dampness)))
+#sa1 <- SpatialPoints(sa1)
+#knn5 <- knn2nb(knearneigh(sa1, k = 5))
+#sapply(1:length(knn5), function(N){mean(dampness[N])})
+#sa1s_na_damp <- sa1_all[which(is.na(sa1_all$dampness)),]
+#sa1s_na_damp$dampness
 
 #### Spatial Interpolation ####
-grid <- st_sample(sa1, 10000, type = "regular")
+grid <- st_sample(sa1, 50000, type = "regular")
 grid <- st_transform(grid, 27291)
 grid <- st_as_sf(grid)
 #st_write(grid, "grid_10000.gpkg")
@@ -77,21 +80,25 @@ tm_shape(grid) +
 
 # IDW for census variables
 idw_income <- idw(formula = median_income~1, locations = sa1_all, 
-               newdata = grid, idp = 1, nmax=10)
+               newdata = grid, idp = 1, nmax=10, na.action=na.pass)
 idw_hs <- idw(formula = no_households~1, locations = sa1_all, 
-                  newdata = grid, idp = 1, nmax=10)
-idw_damp <- idw(formula = dampness~1, locations = sa1_all, 
-                      newdata = grid, idp = 1, nmax=10)
+                  newdata = grid, idp = 1, nmax=10, na.action=na.pass)
 idw_maori <- idw(formula = maori_pr~1, locations = sa1_all, 
-                newdata = grid, idp = 1, nmax=10)
+                newdata = grid, idp = 1, nmax=10, na.action=na.pass)
+idw_damp <- idw(formula = dampness~1, locations = sa1_all, 
+                 newdata = grid, idp = 1, nmax=10, na.action=na.pass)
 
 tm_shape(idw_income) +
   tm_dots(col="var1.pred", style="kmeans") +
   tm_basemap("OpenStreetMap")
 
-idw_joined <- cbind(idw_income, idw_hs, idw_damp, idw_maori)
-colnames(idw_joined) <- c("income", "no_households", "dmapness", "maori_pr")
-idw_joined
+idw_joined <- cbind(idw_income, idw_hs, idw_maori, idw_damp)
+idw_joined = subset(idw_joined, select = -c(var1.var, var1.var.1, var1.var.2,
+                                            var1.var.3, geometry.1, geometry.2,geometry.3))
+
+colnames(idw_joined) <- c("income", "no_households", "maori_pr", "dampness", "geometry")
+st_write(idw_joined,"data/grid_auckaldn_census.gpkg")
+
 #### Distances to transport ####
 train_stations <- stations %>% filter(Mode == "Railway Station")
 bus_stops <- subset(stations, Mode == "Bus Stop")
