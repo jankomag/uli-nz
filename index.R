5d80ea94c8532bc432a6c372cb02f74449802ba0
Jan Magnuszewski <janekmagnuszewski@o2.pl>
2022-11-05 20:06:00 +0000

    index work

--- index.R
+++ index.R
@@ -63,28 +63,52 @@ grid_df %>%
   geom_smooth(method="lm")
 
 #### Index Construcion ####
+summary(grid_df)
 
 #normalise columns function
 normalise <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
+reward=1.1
+# indicators
+grid_modified <- grid_df %>% 
+  mutate(station_meas = ifelse(station_dist<=800,
+                           reward*(1-(normalise(station_dist))),  # reward for being within threshold distance
+                           normalise(1-(normalise(station_dist))))) %>% # penalty for being beyond threshold distance
+  mutate(bus_meas = ifelse(bus_dist<=400,
+                           reward*(1-(normalise(bus_dist))), # reward for being within threshold distance
+                           normalise(1-(normalise(bus_dist))))) %>%     # penalty for being beyond threshold distance
+  mutate(intersections_mea = normalise(no_intersections_in_100m)) %>%
+  mutate(conv_st_mea = normalise(1-(normalise(conv_st_dist)))) %>% 
+  mutate(ev_mea = normalise(1-(normalise(ev_dist)))) %>% 
+  mutate(petrol_mea = normalise(1-(normalise(petrol_st_dist)))) %>% 
+  mutate(second_mea = normalise(1-(normalise(second_dist)))) %>% 
+  mutate(primary_mea = normalise(1-(normalise(primary_dist)))) %>% 
+  mutate(childcare_mea = normalise(1-(normalise(childcare_dist)))) %>% 
+  mutate(cinema_mea = normalise(1-(normalise(cinemas_dist)))) %>% 
+  mutate(gallery_mea = normalise(1-(normalise(galleries_dist)))) %>% 
+  mutate(library_mea = normalise(1-(normalise(libraries_dist)))) %>% 
+  mutate(museum_mea = normalise(1-(normalise(museum_dist)))) %>% 
+  mutate(theatre_mea = normalise(1-(normalise(theatre_dist)))) %>% 
+  mutate(biking_mea = normalise(station_meas)) %>% 
+  mutate(kuli = station_meas + bus_meas + intersections_mea + conv_st_mea +
+           ev_mea + petrol_mea + second_mea + primary_mea + childcare_mea +
+           cinema_mea + gallery_mea + library_mea + museum_mea + theatre_mea + biking_mea) %>% 
+  mutate(kuli_norm = normalise(kuli))
+  
 # normalise each column
-grid_df_n <- data.frame(grid_df[22], lapply(grid_df[1:21], normalise))
+#grid_df_n <- data.frame(grid_df[22], lapply(grid_df[1:21], normalise))
 
 # rejoin with geometry
 grid_geom = subset(grid, select = c(id, geom))
-grid_normed <- left_join(grid_geom, grid_df_n, by = c("id"="id"))
+grid_normed <- left_join(grid_geom, grid_modified, by = c("id"="id"))
 
 summary(grid_normed)
 
-grid_normed <- grid_normed %>%
-  mutate(kuli =  station_dist + bus_dist + ev_dist + conv_st_dist - no_intersections_in_100m +
-           petrol_st_dist - biking_len_100m + second_dist + primary_dist + childcare_dist +
-           cinemas_dist + galleries_dist + libraries_dist + museum_dist + theatre_dist)
-
-tmap_mode("plot")
+#tmap_mode("plot")
 tm_shape(grid_normed) +
-  tm_dots(col = "kuli", style = "equal", palette = "-Reds") +
-  tm_xlab("equal") +
-  label(x="equal")
+  tm_dots(col = "kuli", style = "fisher",
+          palette = "Reds") #+
+  #tm_layout(legend.position = c("right", "top"), title=reward), title.position = c('left', 'top'))
+st_write(grid_normed, "data/geographic/grids/grid_with_kuli.gpkg")
 
